82067eba89f0df54310dfe16a411402677e5f961
JadivaM <jmont261@fiu.edu>
2021-07-01 23:56:26 -0400

    add navbar and component with all sections

--- black-cat/src/App.js
+++ black-cat/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
-// import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Nav from './components/Nav';
+
+import MainPage from './pages/MainPage';
 
 function App() {
   return (
     <>
+    <Router>
     <Nav />
+      <Switch>
+      <Route exact path="/" component={MainPage} />
+      </Switch>
+    </Router>
+
     </>
   );
 }
