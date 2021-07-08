792423e04cf1b4a65bae461e5bda5eefa23f8a64
JadivaM <jmont261@fiu.edu>
2021-07-07 23:50:15 -0400

    add video and source tag with imported video

--- black-cat/src/pages/Home.jsx
+++ black-cat/src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import video from '../images/video.mp4';
 
 const Home = () => {
     return (
         <div className="section home-container" id="home">
+             <video autoPlay loop muted className="video">
+            <source src={video} type="video/mp4" />
+            </video>
             <div className="home-page-text-slogan-container">
             <p className="home-page-text-slogan">Bringing Sound System Culture to South Florida</p>
             </div>
