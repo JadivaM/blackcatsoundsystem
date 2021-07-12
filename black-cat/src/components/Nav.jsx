682af38f304982fd0c825fdf52c0932426daec24
JadivaM <jmont261@fiu.edu>
2021-07-11 22:02:57 -0400

    add scroll as event listener for nav background to change on scroll from transparent to black and add hamburger icon for Nav for responsive behaviors

--- black-cat/src/components/Nav.jsx
+++ black-cat/src/components/Nav.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from '../images/logo.png';
-import { Navbar} from "react-bootstrap";
+import { Navbar, Nav} from "react-bootstrap";
 import { NavHashLink as Link } from "react-router-hash-link";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faBars} from '@fortawesome/free-solid-svg-icons';
 
-const Nav = () => {
+const Navigation = () => {
+const [navbar, setNavbar] = useState(false);
+
+const changeBackground = () => {
+    if (window.scrollY >= 100){
+      setNavbar(true)
+    } else {
+      setNavbar(false)
+    }
+}
+
+window.addEventListener("scroll", changeBackground);
+
 
     return (
-        <div className="navbar-container">
+        <div className={navbar ? "navbar-container active" : "navbar-container"}>
           <Navbar
         collapseOnSelect
         expand="lg"
@@ -14,12 +28,14 @@ const Nav = () => {
         bg="navbar"
         variant="dark"
       >
-        <Navbar.Brand id="brand">
+        <Navbar.Brand href="/#home" id="brand">
         <img src={logo} alt="business logo" className="logo"/>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Toggle aria-controls="responsive-navbar-nav">
+        <FontAwesomeIcon icon={faBars}  color="white" size="lg" />
+        </Navbar.Toggle>
         <Navbar.Collapse id="responsive-navbar-nav">
-          <div className="navbar-links-container">
+        <Nav className="navbar-links-container">
           <Link smooth to="/#home" id='text-link'>
             <p className="navbar-link">Home</p>
             </Link>
@@ -32,11 +48,11 @@ const Nav = () => {
             <Link smooth to="/#events" id='text-link'>
             <p className="navbar-link">Events</p>
             </Link>
-          </div>
+            </Nav> 
           </Navbar.Collapse>
           </Navbar>
         </div>
     )
 }
 
-export default Nav;
+export default Navigation;
