b40a0d16e46f7357e415c5b332103d2e1d09d9f4
JadivaM <jmont261@fiu.edu>
2021-07-11 22:00:44 -0400

    remove video as hero using image for simplicity

--- black-cat/src/pages/Home.jsx
+++ black-cat/src/pages/Home.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import video from '../images/video.mov';
 
 const Home = () => {
     return (
         <div className="section home-container" id="home">
-             <video autoPlay loop muted className="video">
-            <source src={video} type="video/mp4" />
-            </video>
             <div className="home-page-text-slogan-container">
             <p className="home-page-text-slogan">Bringing Sound System Culture to South Florida</p>
             </div>
