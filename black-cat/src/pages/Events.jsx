f42f9c7829f7c72740812f1449677dce48a3b091
JadivaM <jmont261@fiu.edu>
2021-07-13 23:51:20 -0400

    add information, icons, and cards from Material UI to present information

--- black-cat/src/pages/Events.jsx
+++ black-cat/src/pages/Events.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from 'react';
+import cat from '../images/cat.png';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import musicIcon from '../images/song.svg';
+import soundsystem from '../images/sound.png';
+import soundIcon from '../images/sound.svg';
 
 const Events = () => {
     return (
         <div className="section" id="events">
-            <p>Events</p>
+            <img className="black-cat" src={cat} alt="black cat logo"/>
+            <h2 className="about-heading-text">What We Offer</h2>
+            <div className="services-cards-container">
+            <Card className="services-card" variant="outlined">
+                <CardContent>
+                <img className="card-icon" src={soundsystem} alt="icon"/>
+                <p className="card-text">
+                    Hand built sound system powered by some of the best drivers on the market 
+                </p>
+                </CardContent>
+            </Card>
+            <Card className="services-card" variant="outlined">
+                <CardContent>
+                <img className="card-icon" src={musicIcon} alt="icon"/>
+                <p className="card-text">
+                Highest quality amplifiers for high fidelity (hi-fi) sound
+                </p>
+                </CardContent>
+            </Card>
+            <Card className="services-card" variant="outlined">
+                <CardContent>
+                <img className="card-icon" src={soundIcon} alt="icon"/>
+                <p className="card-text">
+                Maintaining quality sound throughout events & connecting people through music
+                </p>
+                </CardContent>
+            </Card>
+            </div>
         </div>
     )
 }
