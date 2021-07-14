5d292a4b1fadc9a10b7971c6cce0e123e8e92c6f
JadivaM <jmont261@fiu.edu>
2021-07-13 23:46:16 -0400

    add NavHashLink for button in home page linking to about us page in mobile/tablet devices

--- black-cat/src/pages/Home.jsx
+++ black-cat/src/pages/Home.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
-
+import { NavHashLink as Link } from "react-router-hash-link";
 
 const Home = () => {
     return (
         <div className="section home-container" id="home">
+            <div className="mobile-hero-image-section">
             <div className="home-page-text-slogan-container">
-            <p className="home-page-text-slogan">Bringing Sound System Culture to South Florida</p>
+            <h1 className="home-page-text-slogan">Bringing Sound System Culture to South Florida</h1>
+            <Link smooth to="/#contact">
             <Button id="mobile-contact-button">Contact us
             </Button>
+            </Link>
+            
             </div>
+            </div>
+            
         </div>
     )
 }
