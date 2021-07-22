6458c96c014307a53008dad6c5022df536644407
JadivaM <jmont261@fiu.edu>
2021-07-22 16:24:18 -0400

    remove div for mobile hero image will no longer be using will be keeping the same styling across different devices with home page

--- black-cat/src/pages/Home.jsx
+++ black-cat/src/pages/Home.jsx
@@ -4,17 +4,13 @@ import { NavHashLink as Link } from "react-router-hash-link";
 const Home = () => {
     return (
         <div className="section home-container" id="home">
-            <div className="mobile-hero-image-section">
             <div className="home-page-text-slogan-container">
             <h1 className="home-page-text-slogan">Bringing Sound System Culture to South Florida</h1>
             <Link smooth to="/#contact" style={{textDecoration: 'none'}}>
             <button id="mobile-contact-button">Contact us
             </button>
             </Link>
-            
-            </div>
-            </div>
-            
+            </div>        
         </div>
     )
 }
