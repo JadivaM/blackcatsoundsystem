9da730922d92bc9be0d1d826924930c320ba1897
JadivaM <jmont261@fiu.edu>
2021-07-23 20:43:55 -0400

    add break to slogan text on front page

--- black-cat/src/pages/Home.jsx
+++ black-cat/src/pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <div className="section home-container" id="home">
             <div className="home-page-text-slogan-container">
-            <h1 className="home-page-text-slogan">Bringing Sound System Culture to South Florida</h1>
+            <h1 className="home-page-text-slogan">Bringing Sound System <br /> Culture to South Florida</h1>
             <Link smooth to="/#contact" style={{textDecoration: 'none'}}>
             <button id="mobile-contact-button">Contact us
             </button>
