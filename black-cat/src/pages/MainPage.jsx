47f0fe1373474559321107e807ed2f7f8cc129ca
JadivaM <jmont261@fiu.edu>
2021-07-24 18:07:35 -0400

    Change component name to Services will not have events

--- black-cat/src/pages/MainPage.jsx
+++ black-cat/src/pages/MainPage.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
-import Events from './Events';
+import Services from './Services';
 
 const MainPage = () => {
     return (
         <div>
         <Home />
         <About />
-        <Events />
+        <Services />
         <Contact />
         </div>
     )
