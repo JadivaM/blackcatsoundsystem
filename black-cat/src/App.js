854a16570775d32f05c5412bbe1e11471c2d8b8e
JadivaM <jmont261@fiu.edu>
2021-07-31 22:39:43 -0400

    add loading screen component with image of logo and import to app.js to show when screen is loading

--- black-cat/src/App.js
+++ black-cat/src/App.js
@@ -1,12 +1,22 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Nav from './components/Nav';
 import MainPage from './pages/MainPage';
+import LoadingScreen from './components/LoadingScreen';
 import 'aos/dist/aos.css';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 2500)
+  }, [])
+
   return (
+    <>
+    {loading === false ? (
     <div className="App">
     <Router>
     <Nav />
@@ -15,6 +25,8 @@ function App() {
       </Switch>
     </Router>
     </div>
+    ) : ( <LoadingScreen /> )}
+    </>
   );
 }
 
